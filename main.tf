fb8bd603127a835a97b7dcc07acf0039db48bc2c
sarveshrasam <sarvesh.rasam@gmail.com>
2021-05-10 15:06:45 +0530

    eighth commit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "devops-vm-1" {
-  ami="ami-03d64741867e7bb94"
+  ami="ami-096fda3c22c1c990a"
   instance_type="t2.micro"
   availability_zone="us-east-1a"
   associate_public_ip_address = "true"
